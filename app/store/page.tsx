816e47aa8c94b1a651f4ea3ea48247359a394af8
leeya018 <leeyahav018@gmail.com>
2025-02-23 16:03:36 +0200

    store view tailwindcss

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -55,7 +55,7 @@ export default function Store() {
       >
         Back to Rooms
       </button>
-      <div className="bg-white p-8 rounded-lg shadow-md w-[90%] h-[80vh] ">
+      <div className="bg-white p-8 rounded-lg shadow-md w-[90%] s:min-h-screen  md:h-[80vh]     mt-10">
         <h1 className="text-2xl font-bold mb-4 text-teal-700 flex justify-center">
           Coin Store
         </h1>
@@ -65,16 +65,27 @@ export default function Store() {
               key={planId}
               onClick={() => handlePurchase(planId as PlanId)}
               className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm
-               text-sm font-medium text-white bg-teal-600 hover:bg-teal-700
-                focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 cursor-pointer"
+         text-sm font-medium text-white focus:outline-none cursor-pointer relative"
             >
-              <div className="mx-auto text-xl flex justify-center ">
-                Buy {plan.coins} coins for ${plan.price}
+              {/* Text Overlay (Fixed Position Above the Image) */}
+              <div
+                className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-30 text-black text-2xl font-semibold 
+        z-20 pointer-events-none"
+              >
+                <div className="flex flex-col px-2 py-2 rounded-md text-center">
+                  <div className="whitespace-nowrap">
+                    Get: {plan.coins} coins
+                  </div>
+                  <div>Pay: {plan.price}$</div>
+                </div>
               </div>
+
+              {/* Image (Ensures it's Below the Text) */}
               <Image
                 alt="image coin"
                 src={plan.imageUrl}
-                className="mx-auto bg-center bg-cover w-72 h-64 rounded-lg"
+                className="mx-auto bg-center bg-cover w-72 h-64 rounded-lg hover:ring-4 ring-yellow-500
+         hover:scale-105 ease-in-out duration-300 z-10"
                 width={300}
                 height={300}
               />
